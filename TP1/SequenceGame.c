7c96dd5c56c407b3c2fa1375c7e10d892d455ba1
lamdevhs <lam.dev.hs@gmail.com>
2018-01-18 23:08:51 +0100

    TP1/

--- TP1/SequenceGame.c
+++ TP1/SequenceGame.c
@@ -1,17 +1,37 @@
 #include <time.h>
 #include <stdlib.h>
 
+
 // physical data
 int yellow = 0; // ?????????
 int red = 1;
 
+int iYellow = 2;
+int iRed = 3;
+  // ^ corresponding interrupters
+
+int good = 5; // ????????????
+int bad = 6;
+  // analogy outputs for green/red light
+
 // sequence
 #define N 4
 int seq[N];
-int attempt[N];
-srand(time(NULL));
+int attemptIx = 0; // always will be % N
 
-void randColor(){
+
+// state managers:
+bool attempting = false; // after showing sequence
+bool restart = false; // end game
+
+
+
+
+
+//:::::::::::::::::::::::
+
+srand(time(NULL));
+void randColorSeq(){
   if (rand() % 2 == 0) {
     return yellow;
   }
@@ -25,46 +45,87 @@ void on(int which){
   digitalWrite(which, HIGH);
 }
 
-void light(int which, int bulb){
-  if (bulb == which){
-    on(bulb);
-  }
-  else {
-    off(bulb);
-  }
-}
-
-
 void newSeq(){
   int i;
   for(i = 0; i < N; i++){
-    seq[i] = randColor;
+    seq[i] = randColorSeq();
   }
 }
 
-void playSeq(){
+#define BLINK_N 10
+// ^ how much the diod blinks when losing/winning
+#define BLINK_RATE 300
+
+void showSeq(){
   int i;
   for(i = 0; i < N; i++){
     int which = seq[i];
-    on(which);
-    delay(1000);
-    off(which);
-    delay(100); // to show the switching
+    on(which); delay(1000);
+    off(which); delay(BLINK_RATE);
+      // ^ to make the change more obvious
   }
 }
+
+void ok(){
+  blinkColor(good, 1);
+}
+
+void youWon(){
+  blink(good, BLINK_N);
+}
+void gameOver(){
+  blinkColor(bad, BLINK_N);
+}
+
+void blink(int which, int amount){
+  int i;
+  for (i = 0; i < amount; i++){
+    analogWrite(which, 255);
+    delay(BLINK_RATE);
+  }
+}
+
+
+void onButton(int which){
+  if (!attempting) return;
+    // ^ not the right time, so do nothing
+  
+  if (seq[attemptIx] == which) {
+    ok();
+    attemptIx = (attemptIx + 1) % N;
+    if (attemptIx == 0) { // successful end of attempt
+      attempting = false;
+      youWon();
+      restart = true;  // game restarted
+    }
+  }
+  else {
+    attempting = false; // unsuccessful end of attempt
+    gameOver();
+    restart = true; // game restarted
+  }
+
+
+void onRedButton() {onButton(red)};
+void onYellowButton() {onButton(yellow)};
+
+//:::::::::::::::::::::
 
 void setup(){
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
-  
-}
-
-void recordAttempt(){
-  
+  pinMode(bad, OUTPUT);
+  pinMode(good, OUTPUT);
+  attachInterrupt(iYellow, onYellowButton, RISING);
+  attachInterrupt(iRed, onRedButton, RISING);
 }
 
 void loop(){
+  restart = false;
   newSeq();
-  playSeq();
-  recordAttempt();
+  showSeq();
+  attempting = true;
+  while (!restart) {}
+    // ^ stuck till the attempt ends
+    // either via youWon or gameOver
 }
